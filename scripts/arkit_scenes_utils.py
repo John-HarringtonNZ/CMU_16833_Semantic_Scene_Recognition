37f996ab6365a60b0c0dee213e8f5e32ff82a2e8
John Harrington <harrington.john@husky.neu.edu>
2023-04-13 17:00:46 -0400

    Updates

--- scripts/arkit_scenes_utils.py
+++ scripts/arkit_scenes_utils.py
@@ -26,14 +26,32 @@ def get_data(n_scenes=1):
                 False,
                 None,
                 False)
-    
+        
+    return video_ids_
 
-def transform_3dod(video_id, image_id):
-
+def transform_3dod(scene_annotations, image_file, traj_line, intrinsics_file):
+    """
+    scene_annotations: -> File that contains scene annotations in scene, wrt venue frame
+    image file: -> file of RGB image
+    traj_line: -> traj in format: timestap (1,), rotation, axis-angle (2-4), translation, m (5-7)
+    intrinsics: -> width height focal_length_x focal_length_y principal_point_x principal_point_y
+    """
 
     return
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    get_data(n_scenes=10)
 
+    scene_ids = get_data(n_scenes=10)
+
+    for scene_id in scene_ids:
+        # Get images in relevant directory
+        scene_dir = "../ARKitScenes/data/3dod/Training/{scene_id}/"
+        bbox_annotations = scene_dir + "{scene_dir}_3dod_annotation.json"
+        images = os.listdir(scene_dir + "{scene_dir}_frames/lowres_wide")
+        intrinsics = os.listdir(scene_dir + "{scene_dir}_frames/lowres_wide_intrinsics")
+        traj_file = scene_dir + "{scene_dir}_frames/lowres_wide.traj"
+        traj = open(traj_file, "r").readlines()
+
+        for i in range(len(traj)):
+            transform_3dod(bbox_annotations, images[i], traj[i], intrinsics[i])
