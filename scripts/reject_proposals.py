fixed dropout filters by masking

scripts/reject_proposals.py
@@ -60,7 +60,7 @@ def identity_filter(target_file, proposals, target_traj_line):
 
     return proposals
 
-def volume_comparison_filter(target_file, proposals, target_traj_line, add_dropout=True, add_noise=True, dropout_prob=0.1, noise_std=0.1, threshold=0.1):
+def volume_comparison_filter(target_file, proposals, target_traj_line, add_dropout=True, add_noise=True, dropout_threshold=0.95, noise_std=0.1, cost_threshold=0.1):
     """
     target_file -> string for file location
     proposals -> [Dict{'file_name':xx.png, 'score': 0.9}]
@@ -77,10 +77,12 @@ def volume_comparison_filter(target_file, proposals, target_traj_line, add_dropo
     # Apply target dropout
     if add_dropout:
         before_dropout_length= len(filtered_target_volumes)
+        mask = np.argwhere(np.random.random((filtered_target_volumes.shape)) > dropout_threshold)
+        
         if before_dropout_length > 2:
-            num_to_remove = max(1,int(dropout_prob*before_dropout_length))
-            indices_to_remove = random.sample(range(before_dropout_length), num_to_remove)
-            filtered_target_volumes = np.delete(filtered_target_volumes, indices_to_remove)    
+            # num_to_remove = max(1,int(dropout_prob*before_dropout_length))
+            # indices_to_remove = random.sample(range(before_dropout_length), num_to_remove)
+            filtered_target_volumes = np.delete(filtered_target_volumes, mask)    
     
     # Apply target geometric noise
     if add_noise:
@@ -95,12 +97,15 @@ def volume_comparison_filter(target_file, proposals, target_traj_line, add_dropo
         # Apply proposal dropout
         if add_dropout:
             before_dropout_length= len(proposal_volumes)
+            # make a mask 
+            mask = np.argwhere(np.random.random((proposal_volumes.shape)) > dropout_threshold)
+
             if before_dropout_length > 2:
-                num_to_remove = max(1, int(dropout_prob*before_dropout_length))
-                indices_to_remove = random.sample(range(before_dropout_length), num_to_remove)
-                proposal_volumes = np.delete(proposal_volumes, indices_to_remove)    
+                # num_to_remove = max(1, int(dropout_prob*before_dropout_length))
+                # indices_to_remove = random.sample(range(before_dropout_length), num_to_remove)
+                proposal_volumes = np.delete(proposal_volumes, mask)    
         
-        # if len(filtered_target_annotations) > 0 :
+        # if len(proposal) > 0 :
         #     breakpoint()
 
         # Apply proposal geometric noise
@@ -114,12 +119,9 @@ def volume_comparison_filter(target_file, proposals, target_traj_line, add_dropo
         # Use the Hungarian algorithm to find the optimal assignment of elements. 
         row_ind, col_ind = linear_sum_assignment(cost_matrix)
 
-        # Get the filtered proposals that have a cost less than or equal to the threshold
-        # subset_count = 0
+        # Get the filtered proposals that have a cost less than or equal to the threshold    
         if len(row_ind) == 0:
-            # print("row_ind len is 0")
-            continue
- 
+            filtered_proposals.append(proposal) 
         else:
             costs = np.zeros(len(row_ind))
 
@@ -130,19 +132,9 @@ def volume_comparison_filter(target_file, proposals, target_traj_line, add_dropo
 
             mean_costs = np.mean(costs)
 
-            if mean_costs <= threshold:
+            if mean_costs <= cost_threshold:
                 filtered_proposals.append(proposal)
                 
-        # for k in range(len(row_ind)):
-        #     i = row_ind[k]
-        #     j = col_ind[k]
-
-        #     if cost_matrix[i, j] <= threshold:
-        #         subset_count += 1
-                
-        # if (subset_count == len(row_ind)):
-        #     filtered_proposals.append(proposal)
-
     return filtered_proposals
 
 
@@ -290,7 +282,7 @@ if __name__ == "__main__":
         "--proposals",type=str, default='../DBoW2/build/output.yaml'
     )
     parser.add_argument(
-        "--output_file",type=str, default='Volume__Noisy.yaml'
+        "--output_file",type=str, default='Volume-Noisy-threshold-10.yaml'
     )
     parser.add_argument(
         "--memory-dir", type=str, default='ARKitScenes/memory'